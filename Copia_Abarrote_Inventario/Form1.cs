771d1930911d8ad34ef686fca583f86bf7180601
Alexis Urquidez <162842416+TheCoders24@users.noreply.github.com>
2024-11-09 16:11:19 -0600

    Agregar archivos de proyecto.

---
+++ Copia_Abarrote_Inventario/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Copia_Abarrote_Inventario
+{
+    public partial class Form1 : Form
+    {
+        private CheckBox checkBoxVentasPorMes;
+        private CheckBox checkBoxVentasPorCliente;
+        private CheckBox checkBoxProductosVendidos;
+        private CheckBox checkBoxIngresoDiario;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void ConfigurarControles()
+        {
+            // Inicializar los CheckBox dinámicamente
+            checkBoxVentasPorMes = new CheckBox
+            {
+                Text = "Ventas por Mes",
+                Location = new System.Drawing.Point(10, 10),
+                Checked = true
+            };
+            checkBoxVentasPorMes.CheckedChanged += async (s, e) => await ActualizarGraficoAsync();
+
+            checkBoxVentasPorCliente = new CheckBox
+            {
+                Text = "Ventas por Cliente",
+                Location = new System.Drawing.Point(10, 40),
+                Checked = true
+            };
+            checkBoxVentasPorCliente.CheckedChanged += async (s, e) => await ActualizarGraficoAsync();
+
+            checkBoxProductosVendidos = new CheckBox
+            {
+                Text = "Productos Vendidos",
+                Location = new System.Drawing.Point(10, 70),
+                Checked = true
+            };
+            checkBoxProductosVendidos.CheckedChanged += async (s, e) => await ActualizarGraficoAsync();
+
+            checkBoxIngresoDiario = new CheckBox
+            {
+                Text = "Ingreso Diario",
+                Location = new System.Drawing.Point(10, 100),
+                Checked = true
+            };
+            checkBoxIngresoDiario.CheckedChanged += async (s, e) => await ActualizarGraficoAsync();
+
+            // Agregar los CheckBox al formulario
+            this.Controls.Add(checkBoxVentasPorMes);
+            this.Controls.Add(checkBoxVentasPorCliente);
+            this.Controls.Add(checkBoxProductosVendidos);
+            this.Controls.Add(checkBoxIngresoDiario);
+        }
+        private async Task ActualizarGraficoAsync()
+        {
+            chart.Series.Clear();
+
+            if (checkBoxVentasPorMes.Checked)
+            {
+                var datos = await _reportService.ObtenerVentasPorMesAsync();
+                AgregarDatosAlGrafico("Ventas Totales Por Mes", datos);
+            }
+            if (checkBoxVentasPorCliente.Checked)
+            {
+                var datos = await _reportService.ObtenerVentasPorClienteAsync();
+                AgregarDatosAlGrafico("Ventas Por Cliente", datos);
+            }
+            if (checkBoxProductosVendidos.Checked)
+            {
+                var datos = await _reportService.ObtenerProductosVendidosAsync();
+                AgregarDatosAlGrafico("Productos Vendidos", datos);
+            }
+            if (checkBoxIngresoDiario.Checked)
+            {
+                var datos = await _reportService.ObtenerIngresoDiarioAsync();
+                AgregarDatosAlGrafico("Ingreso Diario", datos);
+            }
+        }
+
+        private void AgregarDatosAlGrafico(string serieNombre, List<(object XValue, object YValue)> datos)
+        {
+            Series series = new Series(serieNombre)
+            {
+                ChartType = SeriesChartType.Column
+            };
+            foreach (var dato in datos)
+            {
+                series.Points.AddXY(dato.XValue, dato.YValue);
+            }
+            chart1.Series.Add(series);
+        }
+    }
+}
---
+++ Copia_Abarrote_Inventario/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Copia_Abarrote_Inventario")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Copia_Abarrote_Inventario")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("ca46362d-2a2d-4b6d-85cb-d02b76db0a16")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
